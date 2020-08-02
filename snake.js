736c37cd61a36559a687a98301c4a3fc1e7e7648
augustinewafula <augustinetreezy@outlook.com>
2020-08-02 13:31:50 +0300

    create a statis snake

---
+++ snake.js
@@ -0,0 +1,21 @@
+export const SNAKE_SPEED = 2;
+const snakeBody = [
+    {x: 11, y: 11},
+    {x: 12, y: 11},
+    {x: 13, y: 11}
+]
+
+export function update() {
+    console.log("update snake");    
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = segment.x;
+        snakeElement.style.gridColumnStart = segment.y;
+        snakeElement.classList.add("snake");
+        gameBoard.appendChild(snakeElement);
+
+    });
+}
