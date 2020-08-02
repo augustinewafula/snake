90a3e620820b8191aa650a756bfcb174fe87186b
augustinewafula <augustinetreezy@outlook.com>
2020-08-02 21:51:25 +0300

    add game over state

--- js/grid.js
+++ js/grid.js
@@ -7,3 +7,10 @@ export function randomGridPosition(){
     }
 
 }
+
+export function outSideGrid(position) {
+    return (
+        position.x < 1 || position.x > GRID_SIZE ||
+        position.y < 1 || position.y > GRID_SIZE 
+    );    
+}
--- js/snake.js
+++ js/snake.js
@@ -34,10 +34,21 @@ export function expandSnake(amount) {
     newSegments += amount;    
 }
 
-export function onSnake(position) {
-    return snakeBody.some(segment => {
+export function onSnake(position, {ignoreHead = false } = {}) {
+    return snakeBody.some((segment, index) => {
+        if (ignoreHead && index === 0) return false;
         return equalPosition(segment, position);
     });
+    
+}
+
+export function getSnakeHead() {
+    return snakeBody[0];
+    
+}
+
+export function snakeIntersection() {
+    return onSnake(snakeBody[0], {ignoreHead: true});
     
 }
 
