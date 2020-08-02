34b61828ca7dfb0e582e50d8a3a63126b7313912
augustinewafula <augustinetreezy@outlook.com>
2020-08-02 20:50:14 +0300

    add food

---
+++ food.js
@@ -0,0 +1,13 @@
+let food = {x: 10, y: 2};
+
+export function update() {
+    
+}
+
+export function draw(gameBoard) {
+    const foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add("food");
+    gameBoard.appendChild(foodElement);
+}
