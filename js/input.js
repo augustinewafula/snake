b82cf006262fc4232cea41b15e6a79aa7b16d4ba
augustinewafula <augustinetreezy@outlook.com>
2020-08-03 12:27:13 +0300

    add swipe imputs

--- js/input.js
+++ js/input.js
@@ -4,20 +4,16 @@ let lastInputDirection = {x: 0, y: 0};
 window.addEventListener('keydown', e => {
     switch (e.key) {
         case "ArrowUp":
-            if (lastInputDirection.y !== 0) break;
-            inputDirection = {x: 0, y: -1};           
+            up();    
             break;
-        case "ArrowDown":
-            if (lastInputDirection.y !== 0) break;
-            inputDirection = {x: 0, y: 1};           
+        case "ArrowDown":   
+            down();      
             break;
-        case "ArrowLeft":
-            if (lastInputDirection.x !== 0) break;
-            inputDirection = {x: -1, y: 0};           
+        case "ArrowLeft": 
+            left()         
             break;
-        case "ArrowRight":
-            if (lastInputDirection.x !== 0) break;
-            inputDirection = {x: 1, y: 0};           
+        case "ArrowRight":      
+            right()    
             break;
     
         default:
@@ -26,7 +22,47 @@ window.addEventListener('keydown', e => {
 
 });
 
+document.addEventListener('swiped-left', function(e) {
+    left();
+});
+
+document.addEventListener('swiped-right', function(e) {
+    right();
+});
+
+document.addEventListener('swiped-up', function(e) {
+    up();
+});
+
+document.addEventListener('swiped-down', function(e) {
+    down();
+});
+
 export function getInputDirection() {
     lastInputDirection = inputDirection;
     return inputDirection;
+}
+
+function up() {
+    if (lastInputDirection.y == 0) {
+        inputDirection = {x: 0, y: -1};
+    }    
+}
+
+function down() {
+    if (lastInputDirection.y == 0) {
+        inputDirection = {x: 0, y: 1};
+    }
+}
+
+function left() {
+    if (lastInputDirection.x == 0) {
+        inputDirection = {x: -1, y: 0};
+    } 
+}
+
+function right() {
+    if (lastInputDirection.x == 0) {
+        inputDirection = {x: 1, y: 0};
+    } 
 }
